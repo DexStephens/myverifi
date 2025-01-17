e5ff6cba12d8fa77b0fcd7f2ff344a63e4ec25a6
DexStephens <dexterfstephens@gmail.com>
2025-01-16 22:00:49 -0700

    Properly add the env for contract address and corresponding typing, and start to work on performing actions on a contract and listening to emitted events. Pull over the abi from the hardhat output.

--- client/src/components/ContractPlay.tsx
+++ client/src/components/ContractPlay.tsx
@@ -1,5 +1,5 @@
 import { PrivateKeyAccount, getContract } from "viem";
-import { lockAbi } from "../utils/lockAbi";
+import { degreeRegistryAbi } from "../utils/degreeRegistryAbi";
 import { publicClient } from "../utils/client";
 import { useState } from "react";
 
@@ -8,23 +8,60 @@ export default function ContractPlay({
 }: {
   account: PrivateKeyAccount;
 }) {
-  const [contract, setContract] = useState(null);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [contract, setContract] = useState<any>(null);
+  const [events, setEvents] = useState<[]>([]);
 
   const pullContract = async () => {
     const contract = getContract({
-      address: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-      abi: lockAbi,
+      address:
+        import.meta.env.CONTRACT_ADDRESS ??
+        "0x5fbdb2315678afecb367f032d93f642f64180aa4",
+      abi: degreeRegistryAbi,
       client: publicClient,
     });
 
-    console.log(contract);
+    setContract(contract);
   };
+
+  // TODO: need a test account to create a university with, and accounts to add degrees to, need to handle events for when events are emitted and display them as they occur
+  const addUniversity = async () => {};
+
+  const assignDegree = async () => {};
+
+  const listenToAddUniversity = async () => {};
+
+  const listenToAssignDegree = async () => {};
 
   return (
     <div>
       <h1>Account Details</h1>
       <p>{JSON.stringify(account)}</p>
-      <button onClick={pullContract}>Pull Contract Information</button>
+      {contract === null ? (
+        <button onClick={pullContract}>Pull Contract Information</button>
+      ) : (
+        <div>
+          <h4>Perform contract actions</h4>
+          <ul>
+            <li>
+              <form action="">
+                <p>Add University</p>
+              </form>
+            </li>
+            <li>
+              <form action="">
+                <p>Add Degree</p>
+              </form>
+            </li>
+          </ul>
+          <p>List of occurred events</p>
+          <ol>
+            {events.map((event) => (
+              <li></li>
+            ))}
+          </ol>
+        </div>
+      )}
     </div>
   );
 }
--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly CONTRACT_ADDRESS: `0x${string}`;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
