4b42ce9d500cc5591c8963c5f3ef944673291b0c
DexStephens <dexterfstephens@gmail.com>
2025-01-20 13:47:10 -0700

    Basic setup for auth controller and service.

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,7 +1,40 @@
 import { Request, Response } from "express";
+import { AuthService } from "../services/auth.service";
 
 export class AuthController {
-  static async login(req: Request, res: Response): Promise<void> {}
+  static async login(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
 
-  static async register(req: Request, res: Response): Promise<void> {}
+    const success = await AuthService.loginUser(email, password);
+
+    if (success) {
+      res.status(401).json({
+        status: "error",
+        message: "Invalid Credentials",
+      });
+    } else {
+      res.json({
+        status: "success",
+        data: {},
+      });
+    }
+  }
+
+  static async register(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+
+    const success = await AuthService.registerUser(email, password);
+
+    if (success) {
+      res.status(400).json({
+        status: "error",
+        message: "Unable to register user",
+      });
+    } else {
+      res.status(201).json({
+        status: "success",
+        data: {},
+      });
+    }
+  }
 }
---
+++ server/src/services/auth.service.ts
@@ -0,0 +1,9 @@
+export class AuthService {
+  static async loginUser(email: string, password: string): Promise<boolean> {
+    return false;
+  }
+
+  static async registerUser(email: string, password: string): Promise<boolean> {
+    return false;
+  }
+}
