793aab6996bfceec7403d9c072563a3a570cfb94
DexStephens <dexterfstephens@gmail.com>
2025-02-11 08:23:58 -0700

    Remove all unnecessary files and packages that we won't use with new setup.

--- server/src/services/subscription.service.ts
+++
@@ -1,8 +0,0 @@
-import { JsonObject } from "@prisma/client/runtime/library";
-import { HolderUserModel } from "../models/holderUser.model";
-
-export class SubscriptionService {
-  static async subscribe(subscription: JsonObject, email: string) {
-    await HolderUserModel.updateUserByEmail(email, { subscription });
-  }
-}
