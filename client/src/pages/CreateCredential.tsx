67a17c21b14c7366a293faa7281e4084e262c73a
Jacob Sargent <95953463+jrsarge@users.noreply.github.com>
2025-02-13 17:14:57 -0700

    Finished Issuer Landing page w buttons to create, issue and view credential

---
+++ client/src/pages/CreateCredential.tsx
@@ -0,0 +1,10 @@
+import HomeHeader from "../components/HomeHeader";
+
+export default function CreateCredential() {
+  return (
+    <>
+      <HomeHeader />
+      <h1>Create Credential Placeholder</h1>
+    </>
+  );
+}
---
+++ client/src/pages/ViewCredentials.tsx
@@ -0,0 +1,10 @@
+import HomeHeader from "../components/HomeHeader";
+
+export default function ViewCredentials() {
+  return (
+    <>
+      <HomeHeader />
+      <h1>View Credentials Placeholder</h1>
+    </>
+  );
+}
