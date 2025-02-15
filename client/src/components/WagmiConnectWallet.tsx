119e298c7fb95e3eae59b65bcf70fd70f07b8e17
DexStephens <dexterfstephens@gmail.com>
2025-02-15 11:55:24 -0700

    Update wagmi connect wallet to call the right endpoint to update a user address after they connect their wallet.

--- client/src/components/WagmiConnectWallet.tsx
+++ client/src/components/WagmiConnectWallet.tsx
@@ -1,9 +1,28 @@
+import { useRef, useEffect } from "react";
 import { useAccount } from "wagmi";
+import { useUser } from "../context/UserContext";
+import { updateUserAddress } from "../utils/user.util";
+import { Address } from "viem";
 import { Account } from "./Account";
 import { Connect } from "./Connect";
 
 export function WagmiConnectWallet() {
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
+  const { user } = useUser();
+  const updatedRef = useRef(false);
+
+  useEffect(() => {
+    const sendAddress = async (email: string, address: Address) => {
+      const response = await updateUserAddress(email, address);
+      if (response) {
+        updatedRef.current = true;
+      }
+    };
+
+    if (!updatedRef.current && isConnected && address && user) {
+      sendAddress(user.email, address);
+    }
+  }, [isConnected, address, user]);
 
   return (
     <div className="container">{isConnected ? <Account /> : <Connect />}</div>
