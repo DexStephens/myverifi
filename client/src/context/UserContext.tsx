fc9e784ffb67540e8ef233d9afd17996d2d70540
DexStephens <dexterfstephens@gmail.com>
2025-02-19 06:18:52 -0700

    Initial client setup of socket with necessary constants involved.

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, useContext, ReactNode, useState } from "react";
 import { User } from "../utils/user.util";
 import { useNavigate } from "react-router";
+import { useSocket } from "../hooks/useSocket";
+import { CONSTANTS } from "../config/constants";
 
 interface UserContextType {
   user: User | null;
@@ -13,6 +15,75 @@ const UserContext = createContext<UserContextType | undefined>(undefined);
 export function UserProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const navigate = useNavigate();
+  useSocket(user?.address, {
+    [CONSTANTS.SOCKET_EVENTS.CONTRACT_CREATION]: ({ contract_address }) => {
+      console.log("contract created in socket eh");
+      setUser((currentUser) => {
+        if (currentUser && currentUser.issuer) {
+          return {
+            ...currentUser,
+            issuer: { ...currentUser.issuer, contract_address },
+          };
+        }
+        return currentUser;
+      });
+    },
+    [CONSTANTS.SOCKET_EVENTS.CREDENTIAL_CREATION]: ({
+      id,
+      name,
+      token_id,
+      issuer_id,
+    }) => {
+      console.log("credential created in socket eh");
+      setUser((currentUser) => {
+        if (
+          currentUser &&
+          currentUser.issuer &&
+          !currentUser.issuer.credential_types.find((ct) => ct.id === id)
+        ) {
+          return {
+            ...currentUser,
+            issuer: {
+              ...currentUser.issuer,
+              credential_types: [
+                ...currentUser.issuer.credential_types,
+                { id, name, token_id, issuer_id },
+              ],
+            },
+          };
+        }
+        return currentUser;
+      });
+    },
+    [CONSTANTS.SOCKET_EVENTS.CREDENTIAL_ISSUANCE]: ({
+      id,
+      holder_id,
+      credential_type_id,
+      credential_type,
+    }) => {
+      console.log("credential issued in socket eh");
+      setUser((currentUser) => {
+        if (
+          currentUser &&
+          currentUser.holder &&
+          !currentUser.holder.credential_issues.find((ci) => ci.id === id)
+        ) {
+          return {
+            ...currentUser,
+            holder: {
+              ...currentUser.holder,
+              credential_issues: [
+                ...currentUser.holder.credential_issues,
+                { id, holder_id, credential_type_id, credential_type },
+              ],
+            },
+          };
+        }
+
+        return currentUser;
+      });
+    },
+  });
 
   const handleLogout = () => {
     setUser(null);
