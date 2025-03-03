8e76c2c648ab662f4956a8247f2f9ed2988c98d8
DexStephens <dexterfstephens@gmail.com>
2025-03-03 08:42:40 -0700

    Get the verify credential process working.

--- client/src/pages/VerifyCredentials.tsx
+++ client/src/pages/VerifyCredentials.tsx
@@ -1,25 +1,40 @@
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import AdditionIcon from "@mui/icons-material/Add";
-import { useState } from "react";
-import { Button } from "@mui/material";
-
-type CredentialRequest = {
-  issuer: string;
-  tokenId: string;
-};
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import {
+  CredentialRequest,
+  getIssuersWithCredentialTypes,
+  verifyCredentials,
+} from "../utils/verify.util";
+import { Issuer } from "../utils/user.util";
 
 export default function VerifyCredentials() {
   //NEEDSWORK: List of Insitutions and credentials, with one email
-  const [issuers, setIssuers] = useState<string[] | null>(null);
+  const [issuers, setIssuers] = useState<Issuer[] | null>(null);
   const [email, setEmail] = useState("");
   const [credentials, setCredentials] = useState<CredentialRequest[]>([
-    { issuer: "", tokenId: "" },
+    { issuerId: -1, tokenId: "" },
   ]);
   const [error, setError] = useState<string | null>(null);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    const fetchIssuers = async () => {};
+    const fetchIssuers = async () => {
+      const response = await getIssuersWithCredentialTypes();
+
+      setIssuers(response.issuers);
+    };
 
     fetchIssuers();
   }, []);
@@ -30,53 +45,210 @@ export default function VerifyCredentials() {
   };
 
   const addCredential = () =>
-    setCredentials((current) => [...current, { issuer: "", tokenId: "" }]);
+    setCredentials((current) => [...current, { issuerId: -1, tokenId: "" }]);
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    //NEEDSWORK: handle submit here
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (email === "") {
+      setError("Email is required");
+      return;
+    }
+    console.log(credentials);
+
+    if (
+      credentials.filter((c) => c.issuerId === -1 || c.tokenId === "").length >
+      0
+    ) {
+      setError("All credentials must be selected");
+      return;
+    }
+
+    setSubmitting(true);
+
+    const response = await verifyCredentials(
+      email,
+      credentials.map(
+        (c) =>
+          issuers
+            ?.find((i) => i.id === c.issuerId)
+            ?.credential_types.find((ct) => ct.token_id === c.tokenId)
+            ?.id as number
+      )
+    );
+
+    if (response.status) {
+      if (response.valid) {
+        setError("Credentials verified");
+      } else {
+        setError("Credentials not verified");
+      }
+    } else {
+      setError(response.error);
+    }
+
+    setSubmitting(false);
   };
 
   if (issuers === null) {
     return <h4>Loading...</h4>;
   }
 
+  //NEEDSWORK: handle submitting state
+
   return (
-    <div>
-      <h1>Verify Credentials page</h1>
-      <form onSubmit={onSubmit}>
-        {credentials.map((credential) => (
-          <div>
-            <Autocomplete
-              disablePortal
-              options={["BYU", "UofU", "UVU"]}
-              sx={{ borderRadius: 2, backgroundColor: "white" }}
-              renderInput={(params) => (
-                <TextField {...params} label="Institution" />
+    <>
+      <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
+        <Card sx={{ backgroundColor: "#f5f5f5" }}>
+          <CardContent>
+            <Stack spacing={3}>
+              <Typography variant="h4" align="center" sx={{ color: "#333" }}>
+                Batch Send Credentials
+              </Typography>
+              {error && (
+                <Typography color="error" align="center">
+                  {error}
+                </Typography>
               )}
-            />
-          </div>
-        ))}
-        <Button
-          variant="outlined"
-          startIcon={<AdditionIcon />}
-          onClick={addCredential}
-        >
-          Add Credential
-        </Button>
-        <TextField
-          fullWidth
-          margin="normal"
-          label="Email"
-          value={email}
-          onChange={handleEmailChange}
-          required
-          sx={{ backgroundColor: "white", borderRadius: 2 }}
-        />
-        <Button type="submit" variant="contained">
-          Verify
-        </Button>
-      </form>
-    </div>
+
+              <form onSubmit={onSubmit}>
+                {credentials.map((credential, idx) => (
+                  <div
+                    key={idx}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                  >
+                    <div style={{ flexGrow: 1 }}>
+                      <Typography align="center" sx={{ color: "#333" }}>
+                        Credential #{idx + 1}
+                      </Typography>
+                      <Autocomplete
+                        disablePortal
+                        options={issuers.map((issuer) => issuer.name)}
+                        value={
+                          issuers.find(
+                            (issuer) => issuer.id === credential.issuerId
+                          )?.name || null
+                        }
+                        sx={{ borderRadius: 2, backgroundColor: "white" }}
+                        renderInput={(params) => (
+                          <TextField {...params} label="Institution" />
+                        )}
+                        onChange={(_, newValue) => {
+                          setCredentials((current) =>
+                            current.map((c, i) =>
+                              i === idx
+                                ? {
+                                    ...c,
+                                    issuerId:
+                                      newValue !== null
+                                        ? (issuers.find(
+                                            (issuer) => issuer.name === newValue
+                                          )?.id as number)
+                                        : -1,
+                                  }
+                                : c
+                            )
+                          );
+                        }}
+                      />
+                      <Autocomplete
+                        disablePortal
+                        options={
+                          issuers
+                            .find((issuer) => issuer.id === credential.issuerId)
+                            ?.credential_types.map((ct) => ct.name) || []
+                        }
+                        sx={{ borderRadius: 2, backgroundColor: "white" }}
+                        renderInput={(params) => (
+                          <TextField {...params} label="Credential Type" />
+                        )}
+                        value={
+                          issuers
+                            .find((issuer) => issuer.id === credential.issuerId)
+                            ?.credential_types.find(
+                              (ct) => ct.token_id === credential.tokenId
+                            )?.name || null
+                        }
+                        onChange={(_, newValue) => {
+                          console.log(
+                            newValue,
+
+                            issuers
+                              .find(
+                                (issuer) =>
+                                  issuer.id === credentials[0].issuerId
+                              )
+                              ?.credential_types.find(
+                                (ct) => ct.name == newValue
+                              )
+                              ?.token_id.toString()
+                          );
+                          setCredentials((current) =>
+                            current.map((c, i) =>
+                              i === idx
+                                ? {
+                                    ...c,
+                                    tokenId:
+                                      newValue !== null
+                                        ? issuers
+                                            .find(
+                                              (issuer) =>
+                                                issuer.id === c.issuerId
+                                            )
+                                            ?.credential_types.find(
+                                              (ct) => ct.name == newValue
+                                            )
+                                            ?.token_id.toString() || ""
+                                        : "",
+                                  }
+                                : c
+                            )
+                          );
+                        }}
+                      />
+                    </div>
+                    {idx !== 0 && (
+                      <IconButton
+                        color="error"
+                        onClick={() =>
+                          setCredentials((current) =>
+                            current.filter((_, i) => i !== idx)
+                          )
+                        }
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    )}
+                  </div>
+                ))}
+                <Button
+                  variant="outlined"
+                  startIcon={<AdditionIcon />}
+                  onClick={addCredential}
+                >
+                  Add Credential
+                </Button>
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  label="Email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                  sx={{ backgroundColor: "white", borderRadius: 2 }}
+                />
+                <Button type="submit" variant="contained">
+                  Verify
+                </Button>
+              </form>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Container>
+    </>
   );
 }
--- client/src/utils/verify.util.ts
+++ client/src/utils/verify.util.ts
@@ -1,6 +1,11 @@
+export type CredentialRequest = {
+  issuerId: number;
+  tokenId: string;
+};
+
 export async function getIssuersWithCredentialTypes() {
   try {
-    const response = await fetch("http://localhost:3000/issuance/issuers", {
+    const response = await fetch("http://localhost:3000/issuances/issuers", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -11,7 +16,38 @@ export async function getIssuersWithCredentialTypes() {
     console.log("Issuer response:", data);
 
     if (data.status === "success") {
-      return { status: true, user: data.data };
+      return { status: true, issuers: data.data };
+    } else {
+      return {
+        status: false,
+        error:
+          typeof data.error === "string" ? data.error : "Invalid credentials",
+      };
+    }
+  } catch (error) {
+    console.error("Failed to login web user:", error);
+    return { status: false, error: "An error occurred" };
+  }
+}
+
+export async function verifyCredentials(
+  email: string,
+  credential_types: number[]
+) {
+  try {
+    const response = await fetch("http://localhost:3000/issuances/verify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, credential_types }),
+    });
+
+    const data = await response.json();
+    console.log("Verify response:", data);
+
+    if (data.status === "success") {
+      return { status: true, valid: data.data.valid };
     } else {
       return {
         status: false,
