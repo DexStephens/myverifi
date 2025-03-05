4fed9abba77d37d7a377f117f99bd5249c37b10a
tannergwood <tg357@byu.edu>
2025-03-04 21:23:50 -0700

    starts to adding a theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -17,51 +17,59 @@ import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import BatchSendCredentials from "./pages/BatchSendCredentials.tsx";
 import VerifyCredentials from "./pages/VerifyCredentials.tsx";
 import Layout from "./components/Layout.tsx";
+import { theme } from "./styles/theme.ts";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <WagmiProvider config={config}>
-          <QueryClientProvider client={queryClient}>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<App />} />
-                <Route path="verify" element={<VerifyCredentials />} />
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
-                <Route
-                  path="dashboard"
-                  element={
-                    <ProtectedRoute>
-                      <Dashboard />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="issuecredential"
-                  element={
-                    <ProtectedRoute>
-                      <IssueCredential />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="batchsend"
-                  element={
-                    <ProtectedRoute>
-                      <BatchSendCredentials />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route path="createcredential" element={<CreateCredential />} />
-                <Route path="viewcredentials" element={<ViewCredentials />} />
-              </Route>
-            </Routes>
-          </QueryClientProvider>
-        </WagmiProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <UserProvider>
+          <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<App />} />
+                  <Route path="verify" element={<VerifyCredentials />} />
+                  <Route path="login" element={<Login />} />
+                  <Route path="register" element={<Register />} />
+                  <Route
+                    path="dashboard"
+                    element={
+                      <ProtectedRoute>
+                        <Dashboard />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="issuecredential"
+                    element={
+                      <ProtectedRoute>
+                        <IssueCredential />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="batchsend"
+                    element={
+                      <ProtectedRoute>
+                        <BatchSendCredentials />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    path="createcredential"
+                    element={<CreateCredential />}
+                  />
+                  <Route path="viewcredentials" element={<ViewCredentials />} />
+                </Route>
+              </Routes>
+            </QueryClientProvider>
+          </WagmiProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
